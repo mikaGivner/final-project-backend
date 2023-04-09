977257a0543ffa834e06e3e4b86bbddc21d79003
mika <mikagivner123@gmail.com>
2023-04-09 22:16:37 +0300

    room

--- server.js
+++ server.js
@@ -40,12 +40,21 @@ io.on("connection", (socket) => {
     arr.push(newName);
     io.to(newPin).emit("participant_added", arr);
   });
-  socket.off("add_participant", (newName, newPin) => {
+  // socket.off("add_participant", (newName, newPin) => {
+  //   const index = arr.indexOf(newName);
+  //   if (index > -1) {
+  //     arr.splice(index, 1);
+  //   }
+  //   io.to(newPin).emit("participant_added", arr);
+  // });
+  // When a participant leaves the room
+  socket.on("leave_room", (newName, newPin) => {
+    // Get the game data from the database
     const index = arr.indexOf(newName);
     if (index > -1) {
       arr.splice(index, 1);
     }
-    io.to(newPin).emit("participant_added", arr);
+    io.to(newPin).emit("participant_left", arr);
   });
 });
 
