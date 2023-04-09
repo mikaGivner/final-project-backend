d3bcd7bec8c3c1bf5c1fff3740ee191640aeb133
mika <mikagivner123@gmail.com>
2023-04-09 16:27:39 +0300

    off

--- server.js
+++ server.js
@@ -39,20 +39,20 @@ io.on("connection", (socket) => {
     socket.broadcast.emit("participant_added", data);
   });
 
-  socket.on("leave_room", async (name) => {
-    const game = await importPlays.findOne({ gamePin: req.params.gamePin });
-    // Remove the participant's name from the list of participants
-    const updatedParticipants = game.participants.filter(
-      (participant) => participant !== name
-    );
-    // Update the game data in the database
-    await importPlays.findOneAndUpdate(
-      { gamePin: req.params.gamePin },
-      { participants: updatedParticipants }
-    );
-    // Emit an event to all clients in the room indicating that a participant has left
-    io.to(pin).emit("participant_left", name);
-  });
+  // socket.on("leave_room", async (name) => {
+  //   const game = await importPlays.findOne({ gamePin: req.params.gamePin });
+  //   // Remove the participant's name from the list of participants
+  //   const updatedParticipants = game.participants.filter(
+  //     (participant) => participant !== name
+  //   );
+  //   // Update the game data in the database
+  //   await importPlays.findOneAndUpdate(
+  //     { gamePin: req.params.gamePin },
+  //     { participants: updatedParticipants }
+  //   );
+  //   // Emit an event to all clients in the room indicating that a participant has left
+  //   io.to(pin).emit("participant_left", name);
+  // });
 });
 
 const PORT = process.env.PORT || 1000;
