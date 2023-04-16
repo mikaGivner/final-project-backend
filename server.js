8b967d7c00a8871ce05f7a3a5f72353caf92765d
mika <mikagivner123@gmail.com>
2023-04-16 17:11:22 +0300

    add astart pGE

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ io.on("connection", (socket) => {
   });
   socket.on("game_started", (isStart, newPin) => {
     console.log("isStart:", isStart);
-    io.to(newPin).emit("isStart", isStart);
+    io.to(newPin).emit("start", isStart);
   });
 
   socket.on("add_participant", (newName, newPin, admin, yourAdmin) => {
